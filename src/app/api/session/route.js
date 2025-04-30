c3be56d78539950c6d1fde032e1ab440584e8a1e
Kiprotich Davis <mylesdavyz@gmail.com>
2025-04-30 15:04:06 +0300

    Deploy troubleshooting

--- src/app/api/session/route.js
+++ src/app/api/session/route.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth";
+import { authOptions } from "../../../lib/auth";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
