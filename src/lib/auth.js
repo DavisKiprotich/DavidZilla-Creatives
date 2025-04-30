670b8abfcb200f85d5dac02aa368dc57137240dc
Kiprotich Davis <mylesdavyz@gmail.com>
2025-04-30 14:45:54 +0300

    Deploy troubleshooting

---
+++ src/lib/auth.js
@@ -0,0 +1,21 @@
+// src/lib/auth.js
+import GoogleProvider from "next-auth/providers/google";
+
+const allowedEmails = ["dzillacreative@gmail.com", "mylesdavyz@gmail.com"];
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      if (session?.user?.email) {
+        session.user.canUpload = allowedEmails.includes(session.user.email);
+      }
+      return session;
+    },
+  },
+};
