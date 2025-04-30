4f5095b8da73319ca65c7fda3ce88f54ecdac6e2
Kiprotich Davis <mylesdavyz@gmail.com>
2025-04-30 15:47:41 +0300

    Deploy troubleshooting

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "../../../../lib/auth";
 
-const handler = async (req, res) => {
-  try {
-    return await NextAuth(authOptions)(req, res);
-  } catch (error) {
-    console.error("AUTH ERROR:", error); // ← log the error
-    throw error;
-  }
-};
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
